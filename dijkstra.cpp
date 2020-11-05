941208b1e9d6fc7009aeed5983fe3af3a365d5e7
cccbeginner <59042912+cccbeginner@users.noreply.github.com>
2020-11-05 13:23:45 +0800

    Create dijkstra.cpp

---
+++ dijkstra.cpp
@@ -0,0 +1,30 @@
+int MAXV = 500000;
+vector<int> edges[MAXV];
+vector<int> weight[MAXV];
+bool vis[MAXN];
+struct state{
+    int length, u;
+    bool operator<(const state &rhs){
+        return length < rhs.length;
+    }
+};
+
+int dijkstra(int from, int to){//return shortest path length
+    memset(vis, 0, sizeof(vis));
+    priority_queue<state, vector<state>, greater<state> > pq;
+    state ori = {.length = 0, .u = from};
+    pq.push(ori);
+    while(!pq.empty()){
+        state tmd = pq.top();
+        pq.pop();
+        if(tmd.u == to)return tmd.length;
+        if(vis[tmd.u])continue;
+        vis[tmd.u] = 1;
+        for(int i = 0; i < edges[tmd.u].size(); ++i){
+            if(vis[edges[tmd.u][i]])continue;
+            state nxt = {.length = tmd.length + weight[tmd.u][i], .u = edges[tmd.u][i]};
+            pq.push(state);
+        }
+    }
+    return -1;//there's no path between two nodes
+}
