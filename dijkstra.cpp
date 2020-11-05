c122b3942e1934c9acf52fef1eff3ed521e0cab9
cccbeginner <59042912+cccbeginner@users.noreply.github.com>
2020-11-05 13:26:46 +0800

    Update dijkstra.cpp

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -1,11 +1,11 @@
-int MAXV = 500000;
+const int MAXV = 500000;
 vector<int> edges[MAXV];
 vector<int> weight[MAXV];
-bool vis[MAXN];
+bool vis[MAXV];
 struct state{
     int length, u;
-    bool operator<(const state &rhs){
-        return length < rhs.length;
+    bool operator>(const state &rhs)const{
+        return length > rhs.length;
     }
 };
 
@@ -23,7 +23,7 @@ int dijkstra(int from, int to){//return shortest path length
         for(int i = 0; i < edges[tmd.u].size(); ++i){
             if(vis[edges[tmd.u][i]])continue;
             state nxt = {.length = tmd.length + weight[tmd.u][i], .u = edges[tmd.u][i]};
-            pq.push(state);
+            pq.push(nxt);
         }
     }
     return -1;//there's no path between two nodes
